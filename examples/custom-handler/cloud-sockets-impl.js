9f01f0d9bfa4cc8f8d1276f4df364368b2a476ed
Keith Strickland <keith@redpillnow.com>
2020-02-16 11:21:00 -0500

    Updated the examples to ensure they actually work

--- examples/custom-handler/cloud-sockets-impl.js
+++ examples/custom-handler/cloud-sockets-impl.js
@@ -1,13 +1,14 @@
 /**
- * A custom message handler
- * @param {WebSocket} ws 
- * @param {Message} msg 
- * @param {MessageDirector} msgDirector 
+ * A custom message handler. The params that will be passed to this
+ * function are documented below
+ * @param {WebSocket} ws
+ * @param {Message: string} message
+ * @param {MessageDirector} msgDirector
  */
-function customMsgHandler(ws, msg, msgDirector) {
+function customMsgHandler(ws, message, msgDirector) {
+	console.log('Inside customMsgHandler', message);
 	let msg = message;
 	if (typeof message === 'string') {
-		console.log('parsing message')
 		msg = JSON.parse(message);
 	}
 	msg.type = 'announce';
--- examples/custom-handler/index.js
+++ examples/custom-handler/index.js
@@ -1,17 +1,19 @@
 'use strict'
 const express = require('express');
-
-global.server = express();
-
-global.server.use(express.json());
-global.server.use(express.urlencoded({extended: true}));
-
+const http = require('http');
 const socketServer = require('../../index');
 const customHandlers = require('./cloud-sockets-impl');
+
+const app = express();
+global.server = http.createServer();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const socketOptions = {
 	customMsgHandlers: {'customMessageType': customHandlers.customMsgHandler}
 };
-global.server.use(socketServer, {server: global.server, port: 8080}, socketOptions);
+app.use(socketServer(null, socketOptions));
 
 global.server.listen(3000, () =>{
 	console.log(`gcloud-pubsub example listening on port 3000`);
